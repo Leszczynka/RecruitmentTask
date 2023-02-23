8a682a73c841d8b9b9dc8311dd96edadfff4cb47
leszczynka <karolina.leszczynska05@gmail.com>
2023-02-23 19:24:57 +0100

    Create functionality to update posts

--- backend/2.2 Django Blog/blog/views.py
+++ backend/2.2 Django Blog/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Post
 
 class PostListView(ListView):
@@ -14,4 +15,25 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, f'Post has been created successfully.')
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, f'Post has been updated successfully.')
+        return super().form_valid(form)
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != request.user:
+            messages.warning(self.request, 'You have no permission to edit this post!!!')
+            return redirect('blog-posts')
+        else:
+            return super().dispatch(request, *args, *kwargs)
+
+
+
