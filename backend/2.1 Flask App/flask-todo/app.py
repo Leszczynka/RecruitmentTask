13c82b06643008be8a150076bd96ea20635e90e7
leszczynka <karolina.leszczynska05@gmail.com>
2023-02-24 14:03:16 +0100

    Initialize flask app, set up database

---
+++ backend/2.1 Flask App/flask-todo/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+db = SQLAlchemy(app)
+
+
+class ToDoList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(250), nullable=False)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # db.create_all()
+    app.run(debug=True)
