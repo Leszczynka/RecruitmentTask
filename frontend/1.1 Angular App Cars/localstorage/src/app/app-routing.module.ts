0541eeba1506d170388455388a37b8fac5fd10e7
leszczynka <karolina.leszczynska05@gmail.com>
2023-02-26 19:39:08 +0100

    Add routes

--- frontend/1.1 Angular App Cars/localstorage/src/app/app-routing.module.ts
+++ frontend/1.1 Angular App Cars/localstorage/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarAddComponent } from './cars/car-add/car-add.component';
+import { CarDetailComponent } from './cars/car-detail/car-detail.component';
+import { CarsComponent } from './cars/cars.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/cars', pathMatch: 'full' },
+  { path: '/cars', component: CarsComponent, 
+      children: [
+        { path: 'add', component: CarAddComponent },
+        { path: ':id', component: CarDetailComponent }
+      ]},
 ];
 
 @NgModule({
